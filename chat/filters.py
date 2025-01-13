ecd954f9e051a37c05535cd19b02a1121a8be5db
Kaique Mota <motamendes.kaique.100@gmail.com>
2025-01-13 19:51:58 -0300

    Changing django filter fields name of snake_case to camelCase

--- chat/filters.py
+++ chat/filters.py
@@ -7,13 +7,13 @@ class ContactFilter(django_filters.FilterSet):
     name = django_filters.CharFilter(lookup_expr="exact")
     email = django_filters.CharFilter(lookup_expr="exact")
     phone = django_filters.CharFilter(lookup_expr="exact")
-    edited_name = django_filters.CharFilter(lookup_expr="exact")
-    start_date = django_filters.DateFilter(
+    editedName = django_filters.CharFilter(lookup_expr="exact")
+    startDate = django_filters.DateFilter(
         field_name="created_at",
         lookup_expr="gte",
         label="Start date",
     )
-    end_date = django_filters.DateFilter(
+    endDate = django_filters.DateFilter(
         field_name="created_at",
         lookup_expr="lte",
         label="End date",
@@ -25,7 +25,7 @@ class ContactFilter(django_filters.FilterSet):
             "name",
             "email",
             "phone",
-            "edited_name",
-            "start_date",
-            "end_date",
+            "editedName",
+            "startDate",
+            "endDate",
         )
